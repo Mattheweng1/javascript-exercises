22a305e9fb7f3fe686c12acb45b36e0973990015
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2023-04-27 17:09:35 -0500

    Complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, nextItem) => accumulator + nextItem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, nextItem) => accumulator * nextItem, 1);
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let solution = 1;
+	for (i = x; i >= 2; i--) {
+    solution *= i;
+  }
+  return solution;
 };
 
 // Do not edit below this line
