35023b1e52867af76f3967a884e005bae32aa893
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2023-04-28 11:49:07 -0500

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const peopleWithAge = people.map((person) => {
+        if ('yearOfDeath' in person) {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person.age = new Date().getFullYear() - person.yearOfBirth;
+        }
+        return person;
+    });
+    peopleWithAge.sort((personA, personB) => {
+        return personA.age > personB.age ? -1 : 1;
+    });
+    const oldestPerson = peopleWithAge[0];
+    return oldestPerson;
 };
 
 // Do not edit below this line
