c86ef36fb6692daf0a3e5a9137673394210e4fd3
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2023-04-28 10:10:11 -0500

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const onlyLettersArr = str
+        .toLowerCase()
+        .split("")
+        .filter(c => c >= 'a' && c <= 'z');
+    return onlyLettersArr.join("") === onlyLettersArr.reverse().join("");
 };
 
 // Do not edit below this line
