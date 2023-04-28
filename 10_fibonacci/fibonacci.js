e7b5bc3ac3a98fab685b4fb6b2aa2ac02675f2bb
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2023-04-28 10:40:41 -0500

    Complete exercise (use array instead)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    let fibArr = [0,1,1];
+    for (i = 2; i < n; i++){
+        fibArr.push(fibArr[i - 1] + fibArr[i]);
+    }
+    return fibArr[n];
 };
 
 // Do not edit below this line
